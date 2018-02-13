57d0f427cc7de2c941668a1334123be73068a21d
Etienne Delorieux <etienne.delorieux@gmail.com>
2018-02-13 15:22:26 +0100

    Remove useless prefix 'new' for the new task's route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'tasks#index', as: :index
-  get 'new', to: 'tasks#new', as: :new
+  get 'new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
   get 'task/:id', to: 'tasks#show', as: :task
   get 'task/:id/edit', to: 'tasks#edit', as: :edit_task

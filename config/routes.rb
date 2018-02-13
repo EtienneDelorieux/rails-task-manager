6a2ba6591d1f45652c7886a69f4f86b7de82d620
Etienne Delorieux <etienne.delorieux@gmail.com>
2018-02-13 12:09:41 +0100

    befoire install font awesome

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'task/:id', to: 'tasks#show', as: :task
+  get '/', to: 'tasks#index', as: :index
+  get 'task/new', to: 'tasks#new'
 end

43f009a05057ce88f74938ef279bdf54113f25cf
Etienne Delorieux <etienne.delorieux@gmail.com>
2018-02-13 15:08:08 +0100

    Task Manager rails app without css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'task/:id', to: 'tasks#show', as: :task
   get '/', to: 'tasks#index', as: :index
-  get 'task/new', to: 'tasks#new'
+  get 'new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create'
+  get 'task/:id', to: 'tasks#show', as: :task
+  get 'task/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'task/:id', to: 'tasks#update'
+  delete 'task/:id', to: 'tasks#destroy'
 end
